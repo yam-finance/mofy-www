25c049783432b47d0cc737988f9654606a379f43
byterose <>
2021-08-19 16:36:58 +0200

    Format addresses for check

--- src/routes/gallery/[address].svelte
+++ src/routes/gallery/[address].svelte
@@ -3,7 +3,7 @@
 	import Protected from '$lib/components/Protected/index.svelte';
 	import Gallery from '$lib/components/Gallery/index.svelte';
 	import { selectedAccount } from '$lib/stores/web3-store';
-	import { wallet } from 'zksync';
+	import { ethers } from "ethers";
 </script>
 
 <svelte:head>
@@ -15,14 +15,14 @@
 		<div class="px-16 py-8 sm:py-16 sm:px-4 ">
 			<div>
 				<h3 class="text-3xl font-bold text-black dark:text-white mb-2 text-bold">
-					{#if $page.params.address == $selectedAccount}
+					{#if ethers.utils.getAddress($page.params.address) == $selectedAccount}
 						Your Personal Gallery
 					{:else}
 						Artist Gallery
 					{/if}
 				</h3>
 				<span class="text-black dark:text-white opacity-50">
-					{#if $page.params.address == $selectedAccount}
+					{#if ethers.utils.getAddress($page.params.address) == $selectedAccount}
 						The MoFY NFTs in your wallet
 					{:else}
 						The MoFY NFTs of the artists wallet
