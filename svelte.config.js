1f142b2a506cfee023a9fc295a3af8b3a84d3d2b
ethedev <2432272+ethedev@users.noreply.github.com>
2021-08-31 23:12:21 +0400

    Build ipfs

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import adapter_ipfs from 'sveltejs-adapter-ipfs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter(),
-		ssr: false,
-		appDir: '.',
-		vite: () => ({
-			define: {
-				'process.env.NODE_ENV': {}
-			}
-		})
-	}
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+        adapter: adapter_ipfs({ removeBuiltInServiceWorkerRegistration: true, injectPagesInServiceWorker: true }),
+        ssr: false,
+        vite: () => ({
+            define: {
+                'process.env.NODE_ENV': {}
+            }
+        })
+    }
 };
 
 export default config;
