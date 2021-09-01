98f6ff98c4f3434b38d293f638e8e2dff023acf7
byterose <>
2021-09-01 21:03:06 +0200

    Push css fix

--- src/lib/components/ProfileForm/index.svelte
+++ src/lib/components/ProfileForm/index.svelte
@@ -74,7 +74,7 @@
 										type="text"
 										name="company-website"
 										id="company-website"
-										class="focus:ring-white-500 focus:border-white-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+										class="focus:ring-white-500 focus:border-white-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
 										placeholder="Artist Name"
 									/>
 								</div>
