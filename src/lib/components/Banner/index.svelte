94f026ed6eed0aa8fae2036bddfd23cb3984512d
byterose <byterose@tutanota.com>
2021-09-20 13:49:09 +0200

    Change text color

--- src/lib/components/Banner/index.svelte
+++ src/lib/components/Banner/index.svelte
@@ -8,7 +8,7 @@
 			<div class="pr-16 sm:text-center sm:px-16">
 				<p class="font-medium text-white text-center">
 					<span class="md:inline">
-						This site is still a work in <a href="https://docs.yam.finance/projects/mofy/beta">progress</a> - things might change!
+						This site is still a work in <a class="text-black" href="https://docs.yam.finance/projects/mofy/beta">progress</a> - things might change!
 					</span>
 				</p>
 			</div>
